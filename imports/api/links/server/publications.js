39cd8cb431c8328b0d0620d499f41cb3a522814f
Md. Shahjalal <shahjalaldk@gmail.com>
2020-10-28 14:09:59 +0600

    add log

--- imports/api/links/server/publications.js
+++ imports/api/links/server/publications.js
@@ -3,6 +3,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../links.js';
 
+//simple example for publications
 Meteor.publish('links.all', function () {
   return Links.find();
 });
